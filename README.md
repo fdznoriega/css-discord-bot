b55244dbee55e14729fd13ba440c316ad9a32c56
fdznoriega <andresfdz7@gmail.com>
2020-11-28 17:08:24 -0500

    update

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # BCCSS: Building a Discord Bot
 
+Workshop will take place on Thursday, December 3 at 7:00 pm est
+
 ## Instructions
 * Download or fork this repository
 * Create a [Discord account](http://www.discord.com/)
--- index.js
+++ index.js
@@ -4,16 +4,40 @@ const {token, prefix} = require('./config.json');
 const Discord = require('discord.js'); // use the discord.js module
 const client = new Discord.Client(); // create a new discord client that the bot will use
 
-// when the client is ready, fun this code
+// when the client is ready, run this code
 client.once('ready', () => {
 	console.log('Ready!');
 });
 
 // listen for messages!
 client.on('message', (message) => {
-    if(message.content === `${prefix}ping`) {
-        message.channel.send("pong!")
+
+    // lets check the author and the presence of the prefix
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    // slice removes characters from a string
+    // trim removes white space from the sides
+    // split creates an array from that string
+
+    // NEW CODE
+    if(command === 'beep') {
+        message.channel.send("boop");
     }
+    else if(command === 'ping') {
+        message.channel.send("pong");
+    }
+
+    // OLD CODE
+    // if(message.content === `${prefix}ping`) {
+    //     message.channel.send("pong!")
+    // }
+    // else if(message.content === `${prefix}beep`) {
+    //     message.channel.send("boop");
+    // }
+
 });
 
 // log in to discord with the token
